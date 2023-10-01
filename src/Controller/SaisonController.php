efb4320338cea457b880b59fb48a48ff9dfccf8c
k1llerhand19 <135227972+k1llerhand19@users.noreply.github.com>
2023-10-02 00:08:40 +0200

    affichagepdf

--- src/Controller/SaisonController.php
+++ src/Controller/SaisonController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ImageFormType;
+
+use App\Entity\Saison;
+use App\Repository\SaisonRepository;
+
 
 class SaisonController extends AbstractController
 {
     #[Route('/saison/calendrier', name: 'saison.calendrier')]
-    public function index(): Response
+    public function index(SaisonRepository $calendrierRepository): Response
     {
+        $showCalendrier  = $calendrierRepository->findOneBy([],['id' => 'DESC']);
         return $this->render('saison/index.html.twig', [
             'controller_name' => 'SaisonController',
+            'showCalendrier'=>$showCalendrier
         ]);
     }
 }
